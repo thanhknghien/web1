41cc0120fd01880ddc13614cc765b408ee68df92
thanhknghien <thanh2005dinh@gmail.com>
2024-10-21 10:46:52 +0700

    thanh

--- test_cua_thanh/index.js
+++ test_cua_thanh/index.js
@@ -1,1 +1,6 @@
-document.getElementsById('test').innerText = '123';
+function handleChange(){
+  const ele = document.getElementsByClassName("resul");
+  for(let i = 0 ; i< ele.length;i++ ){
+    ele[i].style.backgroundColor= 'aqua';
+  }
+}
