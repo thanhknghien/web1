38ebded128d02d10928a9ecc2da370c24c355a77
mtri <mtri.sgu.cntt0502@gmail.com>
2024-10-21 10:20:54 +0700

    tri upload

--- admin.js
+++ admin.js
@@ -73,3 +73,16 @@ function setPositionByIndex() {
   carouselWrapper.style.transition = 'transform 0.5s ease-in-out'; 
   setCarouselPosition();
 }
+const loginBtn = document.getElementById('login-btn');
+        const loginFormContainer = document.getElementById('login-form-container');
+
+        // Hàm hiển thị/ẩn form đăng nhập
+        function toggleLoginForm() {
+            loginFormContainer.classList.toggle('show');
+        }
+
+        // Lắng nghe sự kiện click vào nút Đăng Nhập
+        loginBtn.addEventListener('click', function(event) {
+            event.preventDefault(); // Ngăn chặn chuyển hướng mặc định
+            toggleLoginForm(); // Hiển thị form đăng nhập
+        });
